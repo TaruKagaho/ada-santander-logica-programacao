16c38b7ea77ef2d83f5191a19b51212333956674
Silva Filho <tarukagahobenu@gmail.com>
2023-07-12 23:43:34 -0300

    Exercício da aula 01.

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static String convertFahreinheitToCelcius(String fahreinheit) {
+        double fahreinheitDouble = Double.parseDouble(fahreinheit);
+        double celcius = 5 * ((fahreinheitDouble - 32) / 9);
+        String result = String.format("%.2f", celcius);
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite um valor de temperatura em Fahrenheit:");
+
+        String temperature = sc.nextLine();
+        sc.close();
+
+        System.out.println(
+                "O valor da temperatura em Celcius: " +
+                        convertFahreinheitToCelcius(temperature)
+        );
+    }
+}
