c5b5cf4447d7aaf4376074f106e7d2701ffb2391
Silva Filho <tarukagahobenu@gmail.com>
2023-07-19 19:53:15 -0300

    feat: resolucão exercício custo

---
+++ src/aula03/Costs.java
@@ -0,0 +1,38 @@
+package aula03;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Arrays;
+
+public class Costs {
+    public static double[] custosCarro( double[] input) {
+        double[] costs = new double[2];
+
+        double costDistributor = (input[1] / input[0]) * 100;
+        double costTax = (input[2] / input[0]) * 100;
+
+        // costs[0] = (double) Math.round(costDistributor * 100) / 100;
+        // costs[1] = (double) Math.round(costTax * 100) / 100;
+
+        /*costs[0] = new BigDecimal((costDistributor * 100) / 100)
+                .setScale(3, RoundingMode.DOWN)
+                .setScale(2, RoundingMode.HALF_DOWN)
+                .doubleValue();*/
+        costs[0] = BigDecimal.valueOf((costDistributor * 100) / 100)
+                .setScale(3, RoundingMode.DOWN)
+                .setScale(2, RoundingMode.HALF_DOWN)
+                .doubleValue();
+        costs[1] = BigDecimal.valueOf((costTax * 100) / 100)
+                .setScale(3, RoundingMode.DOWN)
+                .setScale(2, RoundingMode.HALF_DOWN)
+                .doubleValue();
+
+        return costs;
+    }
+
+    public static void main(String[] args) {
+        double[] costsCar = {100000.00, 12000.00, 20000.00};
+
+        System.out.println("O valor total da comprar é " + Arrays.toString(custosCarro(costsCar)));
+    }
+}
