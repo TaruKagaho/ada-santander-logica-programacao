7e16f8cc2ee021a355ab0e4d28dde1841a27229b
Silva Filho <tarukagahobenu@gmail.com>
2023-07-19 21:15:38 -0300

    feat resolucao exercício caracter único.

---
+++ src/aula04/UniqueCharacter.java
@@ -0,0 +1,63 @@
+package aula04;
+
+public class UniqueCharacter {
+    public static void main(String[] args) {
+        String[] entradas = {
+                "amor",
+                "cocada",
+                "mora",
+                "ovo",
+                "cachorro",
+                "kaka",
+                "saudades",
+                "palalelepipedo",
+                "fofocalizando",
+                "papagaio",
+                "ateiaateiab"
+        };
+
+        int[] saidas = {
+                0,
+                1,
+                0,
+                1,
+                1,
+                -1,
+                2,
+                9,
+                4,
+                4,
+                10
+        };
+
+        for (int i = 0; i < entradas.length; i++) {
+            double resultado = primeiroCaractereUnico(entradas[i]);
+            double esperado = saidas[i];
+
+            System.out.println("Resultado: " + resultado);
+            System.out.println("Esperado: " + esperado);
+            System.out.println(resultado == esperado);
+            System.out.println();
+        }
+    }
+
+    public static int primeiroCaractereUnico(String input) {
+        String[] letters = input.toLowerCase().split("");
+
+        for (String letter : letters) {
+            int count = 0;
+
+            for (String ch : letters) {
+                if (ch.equals(letter)) {
+                    count++;
+                }
+            }
+
+            if (count == 1) {
+                return input.indexOf(letter);
+            }
+        }
+
+        return -1;
+    }
+}
